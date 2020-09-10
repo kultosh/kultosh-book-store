1f676416b23b0617cedd0c34b54c48fc33f56722
kultosh <kulgole90@gmail.com>
2020-09-10 22:20:06 +0545

    Extracted data in index and created search using RegEx

--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -4,8 +4,18 @@ const Author = require('../models/author');
 
 // Author Routes
 
-router.get('/', (req, res) => {
-    res.render('authors/index');
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name !== '')
+    {
+        searchOptions.name = new RegExp(req.query.name, 'i');
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {authors: authors, searchOptions: req.query})
+    } catch (error) {
+       res.redirect('/'); 
+    }
 });
 
 router.get('/create', (req, res) => {
