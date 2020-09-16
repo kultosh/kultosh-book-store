94c0b0429bef8cf2c910918731e130477658e722
kultosh <kulgole90@gmail.com>
2020-09-17 00:03:39 +0545

    Created form to search the book according to date and title

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -5,15 +5,28 @@ const fs = require('fs');
 const uploadPath = path.join('public', Book.coverImageBasePath);
 
 const index = async (req, res) => {
+    let query = Book.find();
+    if (req.query.title != null && req.query.title !== '') {
+        query = query.regex('title', new RegExp(req.query.title, 'i'));
+    }
+    if(req.query.publishedBefore != null && req.query.publishedBefore != '' )
+    {
+        query = query.lte('publishDate', req.query.publishedBefore);
+    }
+    if(req.query.publishedAfter != null && req.query.publishedAfter != '' )
+    {
+        query = query.gte('publishDate', req.query.publishedAfter);
+    }
     try {
         const authors = await Author.find();
-        const books = await Book.find();
+        const books = await query.exec();
         res.render('books/index', {
             authors: authors,
-            books: books
+            books: books,
+            searchOptions: req.query
         });
     } catch (error) {
-        console.log(error);
+        res.redirect('/');
     }
 }
 
